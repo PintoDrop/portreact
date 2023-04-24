028c989ee9a12a0082876a4a21e746f7f024bb96
PintoDrop <mikebender22@gmail.com>
2023-04-24 13:35:36 -0700

    taking out non routes to load page

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,14 +6,14 @@ function Nav(props) {
  <nav class="navbar navbar-inverse navbar-fixed-top">
    <div class="container-fluid">
      <div class="navbar-header">
-       <a class="navbar-brand" href="#">
+       {/* <a class="navbar-brand" href="#">
          Michael's Portfolio
-       </a>
+       </a> */}
      </div>
      <ul class="nav navbar-nav">
-       <li class="active">
+       {/* <li class="active">
          <a href="#">Home</a>
-       </li>
+       </li> */}
        <li>
         <NavLink to="/aboutme">About Me</NavLink>
        </li>
