7d5c3758f85815a639728c8e33d82d189fb2d8c6
PintoDrop <mikebender22@gmail.com>
2023-04-24 19:34:48 -0700

    working on contact form

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -23,6 +23,49 @@ function Contact() {
       setEmail(inputValue);
     }  
   };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    if (!validateEmail(email)) {
+      setErrorMessage("Please enter a valid email");
+      return;
+    }
+  };
+  setEmail('');
+
+  return (
+    <div>
+      <form className="form">
+        <input value={message} 
+        name="message"
+        onChange={handleInputChange}
+        type="message"
+        placeholder="Enter a message!"/>
+
+        <input 
+        value={name}
+        name="name"
+        onChange={handleInputChange} type="name"
+        placeholder="Enter your name"/>
+
+        <input value={email}
+        name="email"
+        onChange={handleInputChange}
+        type="email"
+        placeholder="Enter your email"/>
+
+        <button type="button" onclick={handleFormSubmit}>Send Message!</button>
+      </form>
+      {/* {errorMessage && 
+        <div>
+          <p className="error-text">
+            {errorMessage}
+          </p>
+      } */}
+      
+    </div>
+  )
 }
 
 
