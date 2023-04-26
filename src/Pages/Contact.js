5d2329635607c38764ba2d112e19eccad1e38f94
PintoDrop <mikebender22@gmail.com>
2023-04-26 12:50:23 -0700

    working on contact page

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -2,60 +2,65 @@ import React, { useState } from "react";
 import Form from 'react-bootstrap/Form';
 import Button from "react-bootstrap/Button";
 // import Card from "react-bootstrap/Card";
-// import { validateEmail } from "../utils/helper";
+import { validateEmail } from "../utils/helper";
 
 function Contact() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  // const [isSubmitted, setIsSubmitted] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const { target } = e;
-        const inputType = target.name;
-        const inputValue = target.value;
+    if (!email || !name || !message) {
+      setErrorMessage("All fields must be filled out");
+      return;
+    }
 
-        if (inputType === 'email') {
-          setEmail(inputValue);
-        } else if (inputType === 'name'){
-          setName(inputValue);
-        } else {
-          setMessage(inputValue);
-        }
-        // const handleFormSubmit = (e) => {
-//     e.preventDefault();
-
-    // if (!validateEmail(email)) {
-//       setErrorMessage("Please enter a valid email");
-//       return;
-    // }
-//   };
-//   setEmail('');
+    if(!validateEmail(email)) {
+      setErrorMessage("Must enter a valid email");
+      return;
+    }
+    validateEmail();
+    // setIsSubmitted(true);
   };
 
+
+
   return (
-    
-      // <Card style={{ width: "18rem" }} className="contact">
-      //    <Card.Body>
     <Form className="contactForm" onSubmit={handleSubmit}>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-     <Form.Label>Email address</Form.Label>
-     <Form.Control type="email" placeholder="What's your emai?" />
-  </Form.Group>
+      {errorMessage && <div className="error">{errorMessage}</div>}
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control
+          type="email"
+          placeholder="What's your emai?"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </Form.Group>
 
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-     <Form.Label>Name</Form.Label>
-     <Form.Control type="name" placeholder="What's your name?" />
-   </Form.Group>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label>Name</Form.Label>
+        <Form.Control
+          type="name"
+          placeholder="What's your name?"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </Form.Group>
 
-        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-     <Form.Label>What's your message?</Form.Label>
-     <Form.Control
-       as="textarea"
-      placeholder="put your message here"
-      rows={3}
-     />
-  </Form.Group>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Label>What's your message?</Form.Label>
+        <Form.Control
+          as="textarea"
+          placeholder="Enter your message here"
+          rows={3}
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+      </Form.Group>
 
       <Button variant="warning text-center" className="contactBtn">
         Submit
@@ -63,7 +68,6 @@ function Contact() {
     </Form>
     //  </Card.Body>
     //   </Card>
-    
   );
 }
 
